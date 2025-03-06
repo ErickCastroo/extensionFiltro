509ede1132bd491bfef50b50207634719b0a43a1
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-03-06 13:48:36 -0700

    Rename extension and update description in manifest; refactor popup and validator scripts for improved readability and functionality

--- popup.js
+++ popup.js
@@ -3,21 +3,21 @@
  * extrae los folios y envía los datos al contenido de la página para resaltar los folios.
  */
 document.getElementById("resaltarBtn").addEventListener("click", () => {
-  const fileInput = document.getElementById("fileInput");
-  const file = fileInput.files[0];
-  const resaltarBtn = document.getElementById("resaltarBtn");
+  const fileInput = document.getElementById("fileInput")
+  const file = fileInput.files[0]
+  const resaltarBtn = document.getElementById("resaltarBtn")
 
   if (file) {
-    resaltarBtn.disabled = true; // Bloquear el botón después de hacer clic
+    resaltarBtn.disabled = true // Bloquear el botón después de hacer clic
 
-    const reader = new FileReader();
+    const reader = new FileReader()
 
     reader.onload = function (e) {
-      const data = new Uint8Array(e.target.result);
-      const workbook = XLSX.read(data, { type: "array" });
-      const firstSheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[firstSheetName];
-      const folios = XLSX.utils.sheet_to_json(worksheet, { header: 1 }).flat();
+      const data = new Uint8Array(e.target.result)
+      const workbook = XLSX.read(data, { type: "array" })
+      const firstSheetName = workbook.SheetNames[0]
+      const worksheet = workbook.Sheets[firstSheetName]
+      const folios = XLSX.utils.sheet_to_json(worksheet, { header: 1 }).flat()
 
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(
@@ -25,23 +25,24 @@ document.getElementById("resaltarBtn").addEventListener("click", () => {
           { action: "resaltar", folios: folios },
           (response) => {
             if (response && response.success) {
-              document.getElementById("navegacion").style.display = "block";
+              document.getElementById("navegacion").style.display = "block"
 
-              alert("Folios resaltados correctamente.");  
+              alert("Folios resaltados correctamente.")
               // Puedes quitar el alert y mantener solo el Toast
             } else {
-              resaltarBtn.disabled = false;
+              alert("No se encontraron folios en la página.")
+              resaltarBtn.disabled = false
             }
           }
-        );
-      });
-    };
+        )
+      })
+    }
 
-    reader.readAsArrayBuffer(file);
+    reader.readAsArrayBuffer(file)
   } else {
-    alert("Por favor, selecciona un archivo Excel.");
+    alert("Por favor, selecciona un archivo Excel.")
   }
-});
+})
 
 // Agregar eventos para botones de navegación
 document.getElementById("anteriorBtn").addEventListener("click", () => {
@@ -49,15 +50,15 @@ document.getElementById("anteriorBtn").addEventListener("click", () => {
     chrome.tabs.sendMessage(tabs[0].id, {
       action: "navegar",
       direccion: "anterior",
-    });
-  });
-});
+    })
+  })
+})
 
 document.getElementById("siguienteBtn").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {
       action: "navegar",
       direccion: "siguiente",
-    });
-  });
-});
+    })
+  })
+})
--- validator.js
+++ validator.js
@@ -1,16 +1,17 @@
-document.getElementById("fileInput").addEventListener("change", function() {
+document.getElementById("fileInput").addEventListener("change", function () {
   const file = this.files[0]; // Obtiene el archivo seleccionado
   const errorMessage = document.getElementById("error-message");
-  
-  if (file) {
-      const validExtensions = ["xlsx", "xls", "csv"];
-      const fileExtension = file.name.split(".").pop().toLowerCase();
 
-      if (!validExtensions.includes(fileExtension)) {
-          errorMessage.textContent = "Seleccione un archivo válido (.xlsx, .xls, .csv)";
-          this.value = ""; // Resetea el input para evitar el envío de archivos inválidos
-      } else {
-          errorMessage.textContent = ""; // Borra el mensaje si el archivo es válido
-      }
+  if (file) {
+    const validExtensions = ["xlsx", "xls", "csv"];
+    const fileExtension = file.name.split(".").pop().toLowerCase();
+
+    if (!validExtensions.includes(fileExtension)) {
+      errorMessage.textContent =
+        "Seleccione un archivo válido (.xlsx, .xls, .csv)";
+      this.value = ""; // Resetea el input para evitar el envío de archivos inválidos
+    } else {
+      errorMessage.textContent = ""; // Borra el mensaje si el archivo es válido
+    }
   }
 });
