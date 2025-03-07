dbcea6b3949935a343c1550ebb08c8d3689accb5
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-03-07 11:37:43 -0700

    Refactor validator script to improve event listener setup and error handling for file input

--- validator.js
+++ validator.js
@@ -1,17 +1,26 @@
-document.getElementById("fileInput").addEventListener("change", function () {
-  const file = this.files[0]; // Obtiene el archivo seleccionado
+document.addEventListener("DOMContentLoaded", function () {
+  const fileInput = document.getElementById("fileInput");
   const errorMessage = document.getElementById("error-message");
 
-  if (file) {
-    const validExtensions = ["xlsx", "xls", "csv"];
-    const fileExtension = file.name.split(".").pop().toLowerCase();
+  // Verificar si los elementos existen antes de agregar event listeners
+  if (fileInput && errorMessage) {
+    fileInput.addEventListener("change", function () {
+      const file = this.files[0]; // Obtiene el archivo seleccionado
 
-    if (!validExtensions.includes(fileExtension)) {
-      errorMessage.textContent =
-        "Seleccione un archivo válido (.xlsx, .xls, .csv)";
-      this.value = ""; // Resetea el input para evitar el envío de archivos inválidos
-    } else {
-      errorMessage.textContent = ""; // Borra el mensaje si el archivo es válido
-    }
+      if (file) {
+        const validExtensions = ["xlsx", "xls", "csv"];
+        const fileExtension = file.name.split(".").pop().toLowerCase();
+
+        if (!validExtensions.includes(fileExtension)) {
+          errorMessage.textContent =
+            "Seleccione un archivo válido (.xlsx, .xls, .csv)";
+          this.value = ""; // Resetea el input para evitar el envío de archivos inválidos
+        } else {
+          errorMessage.textContent = ""; // Borra el mensaje si el archivo es válido
+        }
+      }
+    });
+  } else {
+    console.error("No se encontró el elemento 'fileInput' o 'error-message'.");
   }
 });
